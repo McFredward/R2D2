7c21198df165fca4c8631824b58365a4c603e59d
Tim-orius <tim.rosenkranz1@t-online.de>
2022-07-30 22:51:41 +0200

    More config changes

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ eps = 1e-3 #Adam optimzer epsilon #TODO: Warum so gross | Adam angucken?
 grad_norm = 40 #maximum value of the total gradient norm, otherwise gradients will be clipped #TODO: Angucken
 batch_size = 128 # <-- GEN
 learning_starts = 100#50000
-save_interval = 100
+save_interval = 10
 target_net_update_interval = 200 # <--GEN
 gamma = 0.997 #Gamme in goal Gleichung | #TODO: Einlesen | GEN?!
 
@@ -21,11 +21,11 @@ gamma = 0.997 #Gamme in goal Gleichung | #TODO: Einlesen | GEN?!
 prio_exponent = 0.9#0.9 #How much Prioritization should be used (alpha) | 0 for no Prioritized replay <-- GEN
 importance_sampling_exponent = 0.6#Bias regularization because of Prioritization | 0.6 <--GEN
 
-training_steps = 5000
+training_steps = 50000
 buffer_capacity = 50000 #<-- GEN
-max_episode_steps = 15000
-actor_update_interval = 25 #<-- Vielleicht GEN
-block_length = 400  # cut one episode to numbers of blocks to improve the buffer space utilization
+max_episode_steps = 2700
+actor_update_interval = 40 #<-- Vielleicht GEN
+block_length = 40  # cut one episode to numbers of blocks to improve the buffer space utilization
 
 amp = False # mixed precision training
 
