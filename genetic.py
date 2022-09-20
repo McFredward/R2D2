2eb87fe16008ae67817ad2d35bf4e361fb307cae
Tim-orius <tim.rosenkranz1@t-online.de>
2022-09-20 20:30:14 +0200

    Re-Add extended parameter choices for genetic

--- genetic.py
+++ genetic.py
@@ -53,9 +53,9 @@ def train(num_actors=config.num_actors, log_interval=config.log_interval):
                 os.remove(os.path.join(os.getcwd(),'logfiles',file))
 
     # Initial setup. Used for the base agent.
-    start_config = {"prio_exp": config.prio_exponent, "prio_bias": config.importance_sampling_exponent,
+    start_config = {"prio_exp": config.prio_exponent, "prio_bias": config.importance_sampling_exponent, "batch size": int(config.batch_size),
                     "lr": config.lr, "dueling": config.use_dueling,"double": config.use_double , "epsilon": config.base_eps,
-                    "player_idx": 0}
+                    "frame skip": int(config.frame_skip), "gamma": config.gamma, "burn in": int(config.burn_in_steps), "player_idx": 0}
 
     # First agent is the same as start config
     agents.append(create_agent_from_config(start_config, 0, multi_conf, num_actors))
@@ -97,22 +97,26 @@ def train(num_actors=config.num_actors, log_interval=config.log_interval):
 
 def create_agent_from_config(conf: dict, generation_idx, multi_conf="", num_actors=config.num_actors):
 
-    r_buffer = ReplayBuffer.remote(conf["player_idx"],generation_idx, alpha=conf["prio_exp"], beta=conf["prio_bias"])
-    learner = Learner.remote(conf["player_idx"],generation_idx, buffer=r_buffer, lr=conf["lr"], use_dueling=conf["dueling"])
+    r_buffer = ReplayBuffer.remote(conf["player_idx"], generation_idx, batch_size=conf["batch size"], alpha=conf["prio_exp"], beta=conf["prio_bias"])
+    learner = Learner.remote(conf["player_idx"], generation_idx, buffer=r_buffer, lr=conf["lr"], use_dueling=conf["dueling"], use_double=conf["double"])
 
     actors_is_running = [is_running_struct.remote() for _ in range(num_actors)]
 
     if config.multiplayer:
-        base_host_actor = Actor.remote(actors_is_running[0],get_epsilon(0, conf["epsilon"]), learner, r_buffer, multi_conf, True, config.pretrain,
-                                       use_dueling=conf["dueling"])
-        actors = [base_host_actor] + [Actor.remote(actors_is_running[i],get_epsilon(i, conf["epsilon"]), learner, r_buffer,
-                                                        "127.0.0.1:5029", False, config.pretrain,
-                                                        use_dueling=conf["dueling"]) for i in range(1,num_actors)]
+        base_host_actor = Actor.remote(actors_is_running[0], get_epsilon(0, conf["epsilon"]), learner, r_buffer, multi_conf, True, config.pretrain,
+                                       use_dueling=conf["dueling"], use_double=conf["double"], frame_skip=conf["frame skip"], gamma=conf["gamma"],
+                                       buffer_burn_in_steps=conf["burn in"])
+        actors = [base_host_actor] + [Actor.remote(actors_is_running[i], get_epsilon(i, conf["epsilon"]), learner, r_buffer,
+                                                   "127.0.0.1:5029", False, config.pretrain,
+                                                   frame_skip=conf["frame skip"], gamma=conf["gamma"],
+                                                   buffer_burn_in_steps=conf["burn in"],
+                                                   use_dueling=conf["dueling"]) for i in range(1,num_actors)]
     else:
-        actors = [Actor.remote(actors_is_running[i],get_epsilon(i, conf["epsilon"]), learner, r_buffer, multi_conf, False,
-                               config.pretrain, use_dueling=conf["dueling"]) for i in range(num_actors)]
+        actors = [Actor.remote(actors_is_running[i], get_epsilon(i, conf["epsilon"]), learner, r_buffer, multi_conf, False,
+                               config.pretrain, use_dueling=conf["dueling"], use_double=conf["double"], frame_skip=conf["frame skip"],
+                               gamma=conf["gamma"], buffer_burn_in_steps=conf["burn in"],) for i in range(num_actors)]
 
-    return [r_buffer, learner, actors, conf,actors_is_running]
+    return [r_buffer, learner, actors, conf, actors_is_running]
 
 
 def generate_children(agent_confs: list[dict], parent_indexes: list, generation_idx):
